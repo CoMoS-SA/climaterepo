17df20eb3a7a891d7db6202fe211c714de2611b5
tstlnz <testalorenzo@outlook.com>
2023-11-10 13:17:39 -0500

    no guide, yes docu

--- pages/3_📚_Documentation.py
+++ pages/3_📚_Documentation.py
@@ -16,19 +16,14 @@ if 'initialized' in st.session_state:
         del st.session_state['initialized']
 
 st.markdown("# The Weighted Climate Data Repository")
-st.markdown("## Guide")
-st.warning("Page under construction!", icon="⚠️")
+st.markdown("## Documentation")
 
 """
-* *geo_resolution*: choose among administrative units GADM0 (countries) and GADM1 (bigger administrative units after countries);
-* *starting_year* and *ending_year*: window size. Notice that each source provides data for a peculiar time window;
-* *time_frequency*: choose among yearly or monthly climate data;
-* *source*: select source of data (available: CRU TS, UDelaware, HERA5, CSIC);
-* *variable*: select variable of interest (available: temperature, precipitation, SPEI);
-* *weight*: weight aggregation values by population density;
-* *threshold_dummy*: True or False. Number of months over a given threshold for each year in the time window;
-* *threshold_kind*: choose among a percentile or an absolute threshold;
-* *threshold*: cutoff value.
+This is the paper that describes the methodology behind the Weighted Climate Data Repository:
+
+[**A unified repository for pre-processed climate data weighted by gridded economic activity**](https://www.google.it)
+
+Please remember to cite it if you use the data in your research!
 """
 
 # Side bar images
