d772522a2630f69120faf52dc03bceb5b2f67a2e
tstlnz <testalorenzo@outlook.com>
2024-04-02 13:25:54 -0400

    added gdp

--- pages/3_📚_Documentation.py
+++ pages/3_📚_Documentation.py
@@ -53,6 +53,18 @@ with st.expander("Aggregated weights"):
         regions = pd.read_csv('./poly/gadm1_weights_values.csv').to_csv().encode('utf-8')
         st.download_button(label="Download GADM1", data=regions, file_name='gadm1_weights_values.csv')
 
+with st.expander("Economic data"):
+    """
+    The GADM0 GDP data are from the [World Bank](https://data.worldbank.org/indicator/NY.GDP.MKTP.CD), and are measured in current dollars. The GADM1 GRP data are from [Wenz et al. (2023)](https://www.nature.com/articles/s41597-023-02323-8), and are measured in 2015 dollars.
+    """
+    cols = st.columns(2)
+    with cols[0]:
+        countries = pd.read_csv('./poly/gadm0_gdp.csv').to_csv().encode('utf-8')
+        st.download_button(label="Download GADM0", data=countries, file_name='gadm0_gdp.csv')
+    with cols[1]:
+        regions = pd.read_csv('./poly/gadm1_gdp.csv').to_csv().encode('utf-8')
+        st.download_button(label="Download GADM1", data=regions, file_name='gadm1_gdp.csv')
+
 # Side bar images
 # st.sidebar.image("Embeds logo.png", use_column_width=True)
 # st.sidebar.image("download.jpeg", use_column_width=True)
